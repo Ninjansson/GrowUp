a1b61371e3cb27d0bb17841273ded20a77f5ff42
Kim Jansson <kimlennart77@gmail.com>
2015-05-05 11:12:25 +0200

    action_settings = menu_settings

--- app/src/main/java/androids/growup/CategoryActivity.java
+++ app/src/main/java/androids/growup/CategoryActivity.java
@@ -35,7 +35,7 @@ public class CategoryActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.menu_settings) {
             return true;
         }
 
