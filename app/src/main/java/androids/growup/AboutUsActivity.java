b03fef333b8475caf498b189395e2df1001ae934
Amelie Hellners <ameliehellners@gmail.com>
2015-05-12 16:02:21 +0200

    Added HomeIcon(Wrong color) + menu Home Click

--- app/src/main/java/androids/growup/AboutUsActivity.java
+++ app/src/main/java/androids/growup/AboutUsActivity.java
@@ -30,6 +30,9 @@ public class AboutUsActivity extends ActionBarActivity {
             case R.id.menu_my_page:
                 startActivity(new Intent(this, MyPageActivity.class));
                 return true;
+            case R.id.menu_home:
+                startActivity(new Intent(this, MainActivity.class));
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
