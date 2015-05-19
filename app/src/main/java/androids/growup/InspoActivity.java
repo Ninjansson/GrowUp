000ac14448fbe7de7bbbb628775b479680fc29fd
inksy <lee.le.carlsson@gmail.com>
2015-05-20 01:15:42 +0200

    Inspo gridview

--- app/src/main/java/androids/growup/InspoActivity.java
+++ app/src/main/java/androids/growup/InspoActivity.java
@@ -1,72 +1,135 @@
 package androids.growup;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * Created by inksy on 2015-05-15.
- */
-public class InspoActivity extends ActionBarActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class InspoActivity extends Activity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_page);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
+        setContentView(R.layout.fragment_inspo);
+
+        GridView gridView = (GridView)findViewById(R.id.gridview);
+        gridView.setAdapter(new MyAdapter(this));
+
+        
+    }
+
+    private class MyAdapter extends BaseAdapter {
+        private List<Item> items = new ArrayList<Item>();
+        private LayoutInflater inflater;
+
+        public MyAdapter(Context context) {
+            inflater = LayoutInflater.from(context);
+
+            items.add(new Item("Image 1", R.drawable.inspo_1));
+            items.add(new Item("Image 2", R.drawable.inspo_1));
+            items.add(new Item("Image 3", R.drawable.inspo_1));
+            items.add(new Item("Image 4", R.drawable.inspo_1));
+            items.add(new Item("Image 5", R.drawable.inspo_1));
         }
-    }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_inspo, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.menu_my_page:
-                startActivity(new Intent(this, MyPageActivity.class));
-                return true;
-            case R.id.menu_aboutus:
-                startActivity(new Intent(this, AboutUsActivity.class));
-                return true;
-            case R.id.menu_settings:
-                startActivity(new Intent(this, SettingsActivity.class));
-                return true;
-            case R.id.menu_home:
-                startActivity(new Intent(this, MainActivity.class));
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
+        @Override
+        public int getCount() {
+            return items.size();
         }
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_inspo, container, false);
-            return rootView;
+        public Object getItem(int i) {
+            return items.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return items.get(i).drawableId;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            View v = view;
+            ImageView picture;
+            TextView name;
+
+            if (v == null) {
+                v = inflater.inflate(R.layout.gridview_item, viewGroup, false);
+                v.setTag(R.id.picture, v.findViewById(R.id.picture));
+                v.setTag(R.id.text, v.findViewById(R.id.text));
+            }
+
+            picture = (ImageView) v.getTag(R.id.picture);
+            name = (TextView) v.getTag(R.id.text);
+
+            Item item = (Item) getItem(i);
+
+            picture.setImageResource(item.drawableId);
+            name.setText(item.name);
+
+            return v;
+        }
+
+        private class Item {
+            final String name;
+            final int drawableId;
+
+            Item(String name, int drawableId) {
+                this.name = name;
+                this.drawableId = drawableId;
+            }
         }
     }
-}
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.menu_my_page, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.menu_aboutus:
+                    startActivity(new Intent(this, AboutUsActivity.class));
+                    return true;
+                case R.id.menu_settings:
+                    startActivity(new Intent(this, SettingsActivity.class));
+                    return true;
+                case R.id.menu_home:
+                    startActivity(new Intent(this, MainActivity.class));
+                    return true;
+                case R.id.menu_inspo:
+                    startActivity(new Intent(this, InspoActivity.class));
+                    return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+            }
+        }
+     }
+
+
