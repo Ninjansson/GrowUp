4823dd0c12ba6ef6bb8322b263b88395c5c1deec
Kim Jansson <kimlennart77@gmail.com>
2015-05-28 16:38:25 +0200

    Försökte fatta jävla Robolectric

--- app/src/test/java/androids/growup/MainActivityTest.java
+++ app/src/test/java/androids/growup/MainActivityTest.java
@@ -19,18 +19,7 @@ import static org.junit.Assert.assertEquals;
 import static org.fest.assertions.api.ANDROID.assertThat;
 import static org.robolectric.Shadows.shadowOf;
 
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
-    public void testImageView(){
-  //      assertEquals("icon_apple", R.id.plant_link, R.drawable.icon_apple);
-
-    }
-=======
->>>>>>> master
-
->>>>>>> Ampelie
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, emulateSdk = 21, manifest = "../app/AndroidManifest.xml")
 public class MainActivityTest {
@@ -45,32 +34,7 @@ public class MainActivityTest {
         }
 
         @Test
-        public void simpleTest() throws Exception {
+        public void setMainActivityTest() throws Exception {
             assertThat(mainActivity).isNotNull();
-        }
-
-        @Test
-         public void testListView() {
-            ListCategoriesAdapter adapter;
-            ArrayList<Category> listCategories = new ArrayList<>();
-
-            Category kryddor = new Category(1, R.drawable.icon_tomato, "Kryddor");
-            Category fruktobar = new Category(2, R.drawable.icon_apple, "Frukter & bär");
-            Category gronsaker = new Category(3, R.drawable.icon_carrot, "Grönsaker");
-            Category atbarablommor = new Category(4, R.drawable.icon_apple, "Ätbara blommor");
-
-            listCategories.add(kryddor);
-            listCategories.add(fruktobar);
-            listCategories.add(gronsaker);
-            listCategories.add(atbarablommor);
-
-
-            adapter = new ListCategoriesAdapter(mainActivity.getApplicationContext(), listCategories);
-            listView.setAdapter(adapter);
-
-            assertEquals("Kryddor", listView.getItemAtPosition(0), kryddor);
-            assertEquals("Frukter & bär", listView.getItemAtPosition(1), fruktobar);
-            assertEquals("Grönsaker", listView.getItemAtPosition(2), gronsaker);
-            assertEquals("Ätbara blommor", listView.getItemAtPosition(3), atbarablommor);
         }
 }
