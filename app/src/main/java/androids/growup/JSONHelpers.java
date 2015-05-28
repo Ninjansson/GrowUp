54dcb9692d067a1a724701d7cc6f4cb43ed9dd49
Kim Jansson <kimlennart77@gmail.com>
2015-05-28 16:59:26 +0200

    Nu skickar MyPage ett object tillbaka till PlantActivity

--- app/src/main/java/androids/growup/JSONHelpers.java
+++ app/src/main/java/androids/growup/JSONHelpers.java
@@ -1,6 +1,8 @@
 package androids.growup;
 
 import android.content.res.AssetManager;
+import android.util.Log;
+
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,6 +11,7 @@ import java.io.Reader;
 import java.util.List;
 import androids.growup.gson.Categories;
 import androids.growup.gson.Category;
+import androids.growup.gson.Plant;
 
 /**
  * Handles all output from our json file.
@@ -36,5 +39,22 @@ public class JSONHelpers {
         Categories categories = gson.fromJson(reader, Categories.class);
 
         return categories.categories;
+    }
+
+    // Returns a plant with certain id
+    public Plant getOnePlant(int id) {
+        Plant myPlant = null;
+        JSONHelpers categories = new JSONHelpers(manager);
+        List<Category> cats = categories.getAllCategories();
+        for (Category category : cats) {
+                for (Plant plant : category.plants) {
+                    if(plant.plant_id == id) {
+                        Log.d("motherfucker", "THIS => " + plant.name);
+                        Log.d("motherfucker", "ID => " + id);
+                        myPlant = plant;
+                    }
+                }
+        }
+        return myPlant;
     }
 }
