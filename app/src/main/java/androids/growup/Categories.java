4a6b5d693bc16dcf116f4e722ed0e329cb2b5b79
Kim Jansson <kimlennart77@gmail.com>
2015-05-28 12:39:44 +0200

    Deletade filer

--- app/src/main/java/androids/growup/Categories.java
+++
@@ -1,7 +0,0 @@
-package androids.growup;
-
-import java.util.List;
-
-public class Categories {
-    public List<Category> categories;
-}
--- app/src/main/java/androids/growup/Response.java
+++
@@ -1,7 +0,0 @@
-package androids.growup;
-
-/**
- * Created by kim.jansson on 2015-05-26.
- */
-public class Response {
-}
