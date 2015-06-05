3cebd7d7036a1b640133a225ee0f215d31f0d5ca
Amelie Hellners <ameliehellners@gmail.com>
2015-06-05 06:46:43 +0200

    Updated test version - not working

---
+++ executorproject/src/androidTest/java/androids/growup/MainActivityTest.java
@@ -0,0 +1,21 @@
+package androids.growup;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * This is a simple framework for a test of an Application.  See
+ * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
+ * how to write and extend Application tests.
+ * <p/>
+ * To run this test, you can type:
+ * adb shell am instrument -w \
+ * -e class androids.growup.MainActivityTest \
+ * androids.growup.tests/android.test.InstrumentationTestRunner
+ */
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    public MainActivityTest() {
+        super("androids.growup", MainActivity.class);
+    }
+
+}
